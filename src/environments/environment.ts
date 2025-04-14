e55b52190df02a1ddfe0bf5e89c56264094d475a
Mohd-Aqib-Khan <uceraqib786@gmail.com>
2025-04-14 18:49:16 +0530

    Modified the environment variable.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   env: 'DEV',
   production: false,
   baseURL: 'http://192.168.39.237:30081',
+  //baseURL: 'http://localhost:5000', //Required for testing on localhost
   // baseURL: 'https://be83-2409-40d6-115a-a4bf-da89-92a3-f76a-faa7.ngrok-free.app',
 };
